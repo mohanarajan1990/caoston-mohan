d55d01ae886245879b9e3bf9609b1711575c4fa0
mohanarajan1990 <99423268+mohanarajan1990@users.noreply.github.com>
2022-09-17 22:53:25 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return <p>this is mohan</p>;
+
+    
+
+
+  }
+}
+
+export default App;
+
---
+++ index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+import App from "./App";
+ReactDOM.render(<App />,document.getElementById("app"));
+
+
+// function App(props) {
+//   console.log(props)
+//   return <p>This from react app</p>
+// }
+
+// ReactDOM.render(<App name="mohan" age={32} active={true} mobile={[1, 2, 3]}
+//   adrees={{ street: "anna" }} />, document.getElementById("app"));
+
